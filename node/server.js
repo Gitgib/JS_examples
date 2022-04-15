8a127fd72f6ea111ac06b794abd11de4d8fb48d1
AidonisGiorgos <36332325+Gitgib@users.noreply.github.com>
2022-04-15 16:02:36 +0300

    Add files via upload

---
+++ node/server.js
@@ -0,0 +1,15 @@
+// include the http module you need
+var http = require("http");
+// access the createServer method in the http object
+http.createServer(function (request, response) {
+
+    // tell the server what kind of file is coming at it
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    // make the server output a message
+    response.write("Welcome to the future of Javascript.");
+
+    // end the server interaction
+    response.end();
+
+}).listen(3000); // listening on port 3000
